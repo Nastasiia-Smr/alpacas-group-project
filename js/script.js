60ef514e2d01049630e8c0e8da5554a608c0fdfc
Nastasiia-Smr <volhvina@outlook.com>
2021-01-31 19:07:59 +0100

    fixed js+html + style changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-toggleButton.addEventListener('click',()=>{
-    navbarLinks.classList.toggle('active')
-})
+window.onload = addEventListenerToMenuButton();
+
+function addEventListenerToMenuButton() {
+    const toggleButton = document.getElementsByClassName('toggle-button')[0];
+    toggleButton.addEventListener('click', () => {
+        toogleMenu();
+    })
+}
+
+function toogleMenu() {
+    const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+    navbarLinks.classList.toggle('active');
+}
